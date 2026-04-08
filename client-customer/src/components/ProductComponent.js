151d0d4082542f5b96b931c5c79f9be166cc73d7
Tuan <tuananh54140@gmail.com>
2026-04-08 14:40:15 +0700

    add style Product

--- client-customer/src/components/ProductComponent.js
+++ client-customer/src/components/ProductComponent.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import withRouter from '../utils/withRouter';
+import './Product.css'; // Import file CSS mới
 
 class Product extends Component {
   constructor(props) {
@@ -14,37 +15,42 @@ class Product extends Component {
   render() {
     const prods = this.state.products.map((item) => {
       return (
-        <div key={item._id} className="inline">
-          <figure>
-            <Link to={`/product/${item._id}`}>
+        <div key={item._id} className="product-card">
+          <Link to={`/product/${item._id}`} className="product-link">
+            <div className="product-image-wrapper">
               <img
                 src={"data:image/jpg;base64," + item.image}
-                width="300px"
-                height="300px"
-                alt=""
+                alt={item.name}
+                className="product-img"
               />
-            </Link>
-            <figcaption className="text-center">
-              {item.name}
-              <br />
-              Price: {item.price}
-            </figcaption>
-          </figure>
+              <div className="view-detail-overlay">View Detail</div>
+            </div>
+            <div className="product-info">
+              <h3 className="product-name">{item.name}</h3>
+              <p className="product-price">
+                {item.price.toLocaleString()} <span className="currency">đ</span>
+              </p>
+            </div>
+          </Link>
         </div>
       );
     });
 
     return (
-      <div className="text-center">
-        <h2 className="text-center">LIST PRODUCTS</h2>
-        {prods}
+      <div className="product-list-container">
+        <h2 className="category-title">
+          {this.props.params.keyword ? `Results for: "${this.props.params.keyword}"` : "Our Delicacies"}
+        </h2>
+        <div className="product-grid">
+          {prods.length > 0 ? prods : <p className="no-product">No sweet treats found...</p>}
+        </div>
       </div>
     );
   }
 
-  componentDidMount() { // first: /product/...
+  // --- Logic API giữ nguyên ---
+  componentDidMount() {
     const params = this.props.params;
-
     if (params.cid) {
       this.apiGetProductsByCatID(params.cid);
     } else if (params.keyword) {
@@ -52,29 +58,24 @@ class Product extends Component {
     }
   }
 
-  componentDidUpdate(prevProps) { // changed: /product/...
+  componentDidUpdate(prevProps) {
     const params = this.props.params;
-
     if (params.cid && params.cid !== prevProps.params.cid) {
       this.apiGetProductsByCatID(params.cid);
-    } 
-    else if (params.keyword && params.keyword !== prevProps.params.keyword) {
+    } else if (params.keyword && params.keyword !== prevProps.params.keyword) {
       this.apiGetProductsByKeyword(params.keyword);
     }
   }
 
-  // apis
   apiGetProductsByCatID(cid) {
     axios.get('/api/customer/products/category/' + cid).then((res) => {
-      const result = res.data;
-      this.setState({ products: result });
+      this.setState({ products: res.data });
     });
   }
 
   apiGetProductsByKeyword(keyword) {
     axios.get('/api/customer/products/search/' + keyword).then((res) => {
-      const result = res.data;
-      this.setState({ products: result });
+      this.setState({ products: res.data });
     });
   }
 }
